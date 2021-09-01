6b2a35b9a03e84bd85c8c0dc127c251864f39401
devAndTeach <64049415+devAndTeach@users.noreply.github.com>
2021-09-01 15:44:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title": "Hello.  Please update me!  "}
+    model = {"title": "Hello.  Happy Wednesday!!  "}
     return render_template('index.html', model=model)
 
 
